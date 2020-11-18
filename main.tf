ffb42a7aeb69c0785bc8b58586fb3f53ce45381c
phili <philiplutley@gmail.com>
2020-11-18 10:36:30 +0000

    better naming; removed tags.

--- main.tf
+++ main.tf
@@ -1,94 +1,64 @@
-# The following terraform builds this AWS VPC example - https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Scenario2.html
-# 1. Builds an AWS VPC
+# The following terraform builds this exact AWS VPC example - https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Scenario2.html
+# 1. Builds an AWS VPC with cidr 10.0.0.0/16
 # 2. Builds 2 Subnets (1 public/1 private)
 # 3. Builds Internet Gateway (igw) & "attach" to VPC - So that instances in public subnet can send requests to the internet
 # 4. Elastic IP + attach to NAT Gateway (ngw) - So that instances in private subnet can send requests to the internet
 # 5. Public Route table & Route table association of public subnet to igw
 # 6. Private Route table & Route table association of private subnet to ngw
 
-# TODO convert to module. ie. paramaterize.
-# TODO Update to Terraform V12
-
 terraform {
-  required_version = "0.11.13"
+  required_version = "~> 0.11"
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "${var.region}"
 }
 
-resource "aws_vpc" "PhilsTestVPC" {
+resource "aws_vpc" "vpcmain" {
   cidr_block = "10.0.0.0/16"
-
-  tags {
-    Name = "Phils Terraform VPC"
-  }
 }
 
 # Public Subnet
 
 resource "aws_subnet" "PublicSub" {
   cidr_block = "10.0.0.0/24"
-  vpc_id     = "${aws_vpc.PhilsTestVPC.id}"
-
-  tags {
-    Name = "Phils Terraform Public Subnet"
-  }
+  vpc_id     = "${aws_vpc.vpcmain.id}"
 }
 
 # Private Subnet
 
 resource "aws_subnet" "PrivateSub" {
   cidr_block = "10.0.1.0/24"
-  vpc_id     = "${aws_vpc.PhilsTestVPC.id}"
-
-  tags {
-    Name = "Phils Terraform Private Subnet"
-  }
+  vpc_id     = "${aws_vpc.vpcmain.id}"
 }
 
 # VPC Internet Gateway
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.PhilsTestVPC.id}"
+  vpc_id = "${aws_vpc.vpcmain.id}"
 
-  tags {
-    Name = "Phils Terraform IGW"
-  }
 }
 
 # NAT Gateway + EIP
 
 resource "aws_eip" "NAT_EIP" {
   vpc = true
-
-  tags {
-    Name = "Phils Terraform NAT EIP"
-  }
 }
 
 resource "aws_nat_gateway" "NATGW" {
   allocation_id = "${aws_eip.NAT_EIP.id}"
   subnet_id     = "${aws_subnet.PublicSub.id}"
   depends_on    = ["aws_internet_gateway.igw"]
-
-  tags {
-    Name = "Phils Terraform NATGW"
-  }
 }
 
 # Route Table - PUBLIC
 
 resource "aws_route_table" "Public" {
-  vpc_id = "${aws_vpc.PhilsTestVPC.id}"
+  vpc_id = "${aws_vpc.vpcmain.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-
-  tags {
-    Name = "Phils Terraform Public Route Table"
   }
 }
 
@@ -100,15 +70,11 @@ resource "aws_route_table_association" "PublicTable" {
 # Route Table - PRIVATE
 
 resource "aws_route_table" "Private" {
-  vpc_id = "${aws_vpc.PhilsTestVPC.id}"
+  vpc_id = "${aws_vpc.vpcmain.id}"
 
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.NATGW.id}"
-  }
-
-  tags {
-    Name = "Phils Terraform Private Route Table"
   }
 }
 
